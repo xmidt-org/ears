7e68868493494a3de7cf3ff6eac7fb2865736441
bwolf200 <mail@boriswolf.com>
2020-10-06 11:37:36 -0700

    file headers

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,20 @@
+/**
+ *  Copyright (c) 2020  Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
@@ -20,7 +37,7 @@ var (
 		"src_type" : "debug",
 		"src_params" :
 		{
-			"rounds" : 1,
+			"rounds" : 10,
 			"interval_ms" : 1000,
 			"payload" : {
 				"foo" : "bar"
@@ -70,8 +87,8 @@ func main() {
 		log.Error().Msg(err.Error())
 		return
 	}
-	buf, _ := json.MarshalIndent(rte, "", "\t")
-	fmt.Printf("%s\n", string(buf))
+	//buf, _ := json.MarshalIndent(rte, "", "\t")
+	//fmt.Printf("%s\n", string(buf))
 	err = rtmgr.AddRoute(ctx, &rte)
 	if err != nil {
 		log.Error().Msg(err.Error())
--- internal/model.go
+++ internal/model.go
@@ -1,3 +1,20 @@
+/**
+ *  Copyright (c) 2020  Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package internal
 
 import (
@@ -7,6 +24,7 @@ import (
 const (
 
 	// plugin types
+	//TODO: where should this live?
 
 	PluginTypeKafka  = "kafka"  // generic kafka plugin
 	PluginTypeKDS    = "kds"    // generic kds plugin
@@ -19,6 +37,7 @@ const (
 	PluginTypeFilter = "filter" // a filter plugin that filters or transforms and event
 
 	// filter types
+	//TODO: where should this live?
 
 	FilterTypeFilter      = "filter"
 	FilterTypeMatcher     = "match"
