782e2b5885aea3c3c27aaddd5aad9506f142ba4c
bwolf200 <mail@boriswolf.com>
2020-10-06 18:16:14 -0700

    code covergae at 42%

--- internal/mem_routing_table_mgr_test.go
+++ internal/mem_routing_table_mgr_test.go
@@ -1,9 +1,122 @@
 package internal_test
 
 import (
+	"context"
+	"encoding/json"
 	"testing"
+
+	"github.com/xmidt-org/ears/internal"
+)
+
+var (
+	ROUTE_1 = `
+	{
+		"orgId" : "comcast",
+		"appId" : "xfi",
+		"userId" : "boris",
+		"source" : {
+			"type" : "debug",
+			"params" :
+			{
+				"rounds" : 3,
+				"intervalMS" : 250,
+				"payload" : {
+					"foo" : "bar"
+				}
+			}
+		},
+		"destination" : {
+			"type" : "debug",
+			"params" : {}
+		},
+		"filterChain" : {
+			"filters": 
+			[
+				{
+					"type" : "match",
+					"params" : {
+						"pattern" : {
+							"foo" : "bar"
+						}
+					}
+				},
+				{
+					"type" : "filter",
+					"params" : {
+						"pattern" : {
+							"hello" : "world"
+						}
+					}
+				},
+				{
+					"type" : "split",
+					"params" : {}
+				},
+				{
+					"type" : "transform",
+					"params" : {}
+				}
+			]
+		},
+		"deliveryMode" : "at_least_once"
+	}
+	`
+	ROUTE_2 = `
+	{
+		"orgId" : "comcast",
+		"appId" : "xfi",
+		"userId" : "boris",
+		"source" : {
+			"type" : "debug",
+			"params" :
+			{
+				"rounds" : 3,
+				"intervalMS" : 250,
+				"payload" : {
+					"foo" : "bar"
+				}
+			}
+		},
+		"destination" : {
+			"type" : "debug",
+			"params" : {}
+		},
+		"deliveryMode" : "at_least_once"
+	}
+	`
 )
 
 func TestSingleRoute(t *testing.T) {
-	//ctx := context.Background()
+	ctx := context.Background()
+	var rtmgr internal.RoutingTableManager
+	// init in memory routing table manager
+	rtmgr = internal.NewInMemoryRoutingTableManager()
+	if rtmgr.GetRouteCount(ctx) != 0 {
+		t.Errorf("routing table not empty")
+		return
+	}
+	var rte internal.RoutingTableEntry
+	err := json.Unmarshal([]byte(ROUTE_1), &rte)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	// add a route
+	err = rtmgr.AddRoute(ctx, &rte)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	// check route
+	if rtmgr.GetRouteCount(ctx) != 1 {
+		t.Errorf("routing table doesn't have expected entry")
+	}
+	allRoutes, err := rtmgr.GetAllRoutes(ctx)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if len(allRoutes) != 1 {
+		t.Errorf("routing table doesn't have expected entry")
+	}
 }
