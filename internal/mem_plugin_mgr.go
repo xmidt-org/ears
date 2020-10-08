dea3cd5d59373c1f01f1fc2806004865ff12c32a
bwolf200 <mail@boriswolf.com>
2020-10-07 22:16:25 -0700

    bug fixes and more tests

--- internal/mem_plugin_mgr.go
+++ internal/mem_plugin_mgr.go
@@ -20,6 +20,9 @@ package internal
 import (
 	"context"
 	"errors"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
 )
 
 //TODO: merge input plugin mgr and output plugin mgr
@@ -69,14 +72,18 @@ func (pm *DefaultInputPluginManager) RegisterRoute(ctx context.Context, rte *Rou
 	plugin := rte.Source
 	hash := plugin.Hash(ctx)
 	if hash == "" {
-		return plugin, errors.New("bad plugin configuration")
+		return plugin, errors.New("empty plugin hash")
 	}
 	if p, ok := pm.pluginMap[hash]; ok {
 		p.RouteCount++
+		if p.routes == nil {
+			p.routes = make([]*RoutingTableEntry, 0)
+		}
 		p.routes = append(p.routes, rte)
 		return p, nil
 	}
 	pm.pluginMap[hash], err = NewInputPlugin(ctx, rte)
+	log.Debug().Msg(fmt.Sprintf("registering input plugin with hash %s", hash))
 	if err != nil {
 		return plugin, err
 	}
@@ -88,14 +95,18 @@ func (pm *DefaultOutputPluginManager) RegisterRoute(ctx context.Context, rte *Ro
 	plugin := rte.Destination
 	hash := plugin.Hash(ctx)
 	if hash == "" {
-		return plugin, errors.New("bad plugin configuration")
+		return plugin, errors.New("empty plugin hash")
 	}
 	if p, ok := pm.pluginMap[hash]; ok {
 		p.RouteCount++
+		if p.routes == nil {
+			p.routes = make([]*RoutingTableEntry, 0)
+		}
 		p.routes = append(p.routes, rte)
 		return p, nil
 	}
 	pm.pluginMap[hash], err = NewOutputPlugin(ctx, rte)
+	log.Debug().Msg(fmt.Sprintf("registering output plugin with hash %s", hash))
 	if err != nil {
 		return plugin, err
 	}
@@ -106,17 +117,23 @@ func (pm *DefaultInputPluginManager) UnregisterRoute(ctx context.Context, rte *R
 	plugin := rte.Source
 	hash := plugin.Hash(ctx)
 	if hash == "" {
-		return errors.New("bad plugin configuration")
+		return errors.New("empty plugin hash")
 	}
 	if p, ok := pm.pluginMap[hash]; ok {
-		p.RouteCount--
-		if p.RouteCount <= 0 {
-			// stop plugin
-		}
+		log.Debug().Msg(fmt.Sprintf("unregistering input plugin with hash %s", hash))
 		routes := make([]*RoutingTableEntry, 0)
-		for _, r := range p.routes {
-			if r != rte {
-				routes = append(routes, r)
+		if p.routes != nil {
+			for _, r := range p.routes {
+				if r != rte {
+					routes = append(routes, r)
+				} else {
+					p.RouteCount--
+				}
+			}
+			if p.RouteCount <= 0 {
+				//TODO: stop plugin
+				log.Debug().Msg(fmt.Sprintf("stopped input plugin with hash %s", hash))
+				delete(pm.pluginMap, hash)
 			}
 		}
 		p.routes = routes
@@ -128,17 +145,23 @@ func (pm *DefaultOutputPluginManager) UnregisterRoute(ctx context.Context, rte *
 	plugin := rte.Destination
 	hash := plugin.Hash(ctx)
 	if hash == "" {
-		return errors.New("bad plugin configuration")
+		return errors.New("empty plugin hash")
 	}
 	if p, ok := pm.pluginMap[hash]; ok {
-		p.RouteCount--
-		if p.RouteCount <= 0 {
-			// stop plugin
-		}
+		log.Debug().Msg(fmt.Sprintf("unregistering output plugin with hash %s", hash))
 		routes := make([]*RoutingTableEntry, 0)
-		for _, r := range p.routes {
-			if r != rte {
-				routes = append(routes, r)
+		if p.routes != nil {
+			for _, r := range p.routes {
+				if r != rte {
+					routes = append(routes, r)
+				} else {
+					p.RouteCount--
+				}
+			}
+			if p.RouteCount <= 0 {
+				//TODO: stop plugin
+				log.Debug().Msg(fmt.Sprintf("stopped output plugin with hash %s", hash))
+				delete(pm.pluginMap, hash)
 			}
 		}
 		p.routes = routes
