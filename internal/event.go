4585a285be96ddf61adb2a4e9a1c5266efe904b3
bwolf200 <mail@boriswolf.com>
2020-10-12 15:42:59 -0700

    reduced access to some event props

--- internal/event.go
+++ internal/event.go
@@ -30,8 +30,8 @@ type (
 		Payload  interface{} `json:"payload"`  // event payload (could also be string, but preparsed i sprobably more efficient if we want to allow deep inspection)
 		Encoding string      `json:"encoding"` // optional encoding hint to be set by input plugin
 		Metadata interface{} `json:"metadata"` // optional metadata produced by filter chain
-		AckTree  AckTree     `json:"ackTree"`  // optional ack chain (or ack tree)
-		Source   Pluginer    `json:"source"`   // pointer to source plugin instance
+		ackTree  AckTree     `json:"ackTree"`  // optional ack chain (or ack tree)
+		source   Pluginer    `json:"source"`   // pointer to source plugin instance
 		TxId     string      `json:"txid"`     // transaction ID (probably also available from context)
 		Ts       int         `json: "ts"`      // timestamp when event was received
 	}
@@ -45,7 +45,7 @@ func NewEvent(ctx context.Context, plugin Pluginer, payload interface{}) *Event 
 	buf := make([]byte, 16)
 	rand.Read(buf)
 	evt.TxId = fmt.Sprintf("%x-%x-%x-%x-%x", buf[0:4], buf[4:6], buf[6:8], buf[8:10], buf[10:])
-	evt.Source = plugin
+	evt.source = plugin
 	evt.Metadata = make(map[string]interface{})
 	return evt
 }
