ba3f70a9ae21e5259578e15553f144342bd21c72
Trevor Gattis <1063149+gtrevg@users.noreply.github.com>
2020-11-09 14:17:58 -0500

    test hash function

--- pkg/hasher/hasher_test.go
+++ pkg/hasher/hasher_test.go
@@ -32,18 +32,23 @@ func TestHash(t *testing.T) {
 		name  string
 		value interface{}
 	}{
-		{name: "string", value: ""},
-		{name: "int", value: "abcdefghijklmnopqrstuvwxyz"},
-		{name: "float", value: "1234567890"},
-		{name: "bytes", value: []byte{1, 2, 3, 4}},
-		{name: "[]string]", value: []string{"1234567890", "0987654321"}},
+		{name: "bool", value: true},
+		{name: "[]bool", value: []bool{true, false}},
+		{name: "string", value: "abcdefgh"},
+		{name: "string_empty", value: ""},
+		{name: "[]string", value: []string{"1234567890", "0987654321"}},
+		{name: "int", value: 42},
+		{name: "[]int", value: []int64{-34, 0, 299792458}},
+		{name: "float", value: 3.14},
+		{name: "[]float", value: []float32{3.14, 2.718}},
+		{name: "[]byte", value: []byte{1, 2, 3, 4}},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			g := goldie.New(t, goldie.WithTestNameForDir(true))
 
-			g.Assert(t, tc.name, []byte(hasher.String(tc.value)))
+			g.Assert(t, tc.name, []byte(hasher.Hash(tc.value)))
 		})
 	}
 }
