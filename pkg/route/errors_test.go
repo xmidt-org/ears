f6ae85f3d5820ef0001b9f8b7588db9efca8edab
Trevor Gattis <1063149+gtrevg@users.noreply.github.com>
2020-12-04 03:03:57 -0500

    route package updates

---
+++ pkg/route/errors_test.go
@@ -0,0 +1,48 @@
+// Copyright 2020 Comcast Cable Communications Management, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package route_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/sebdah/goldie/v2"
+	"github.com/xmidt-org/ears/pkg/route"
+)
+
+func TestErrorMessage(t *testing.T) {
+	testCases := []struct {
+		name string
+		err  error
+	}{
+		{name: "InvalidRouteError", err: &route.InvalidRouteError{}},
+
+		{
+			name: "InvalidRouteError_Err",
+			err:  &route.InvalidRouteError{Err: fmt.Errorf("wrapped error")},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := goldie.New(t, goldie.WithTestNameForDir(true))
+			g.Assert(t, tc.name, []byte(fmt.Sprint(tc.err)))
+			g.Assert(t, tc.name+"_unwrapped", []byte(fmt.Sprint(errors.Unwrap(tc.err))))
+
+		})
+	}
+
+}
--- pkg/route/route_test.go
+++ pkg/route/route_test.go
@@ -55,6 +55,47 @@ func TestErrorCases(t *testing.T) {
 	}
 }
 
+func TestRoutes(t *testing.T) {
+	testCases := []struct {
+		name string
+		r    receiver.Receiver
+		f    filter.Filterer
+		s    sender.Sender
+		err  error
+	}{
+		{
+			name: "nil receiver",
+			r:    nil,
+			f:    &filter.FiltererMock{},
+			s:    &sender.SenderMock{},
+			err:  &route.InvalidRouteError{},
+		},
+		{
+			name: "nil sender",
+			r:    &receiver.ReceiverMock{},
+			f:    &filter.FiltererMock{},
+			s:    nil,
+			err:  &route.InvalidRouteError{},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
+	defer cancel()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			a := NewWithT(t)
+
+			err := (&route.Route{}).Run(ctx, tc.r, tc.f, tc.s)
+			a.Expect(err).ToNot(BeNil())
+			a.Expect(errTypeToString(err)).To(Equal(errTypeToString(tc.err)))
+
+		})
+	}
+}
+
+// =========================================================================
+
 func errTypeToString(err error) string {
 	if err == nil {
 		return "<nil>"
