5ce9fb3662a77dc81caa471e72e347787469210b
bwolf200 <mail@boriswolf.com>
2021-01-14 10:11:16 -0800

    filter route test case

--- cmd/ears/testdata/run.sh
+++ cmd/ears/testdata/run.sh
@@ -8,6 +8,10 @@ curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRoute.js
 
 curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBlankID.json | jq .
 
+curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleFilterRoute.json | jq .
+
+echo "invalid routes"
+
 curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBadName.json | jq .
 
 curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBadPluginName.json | jq .
@@ -31,6 +35,8 @@ echo "delete routes"
 curl -X DELETE http://localhost:3000/ears/v1/routes/2abe4d167883f594f7b48ee7e1d247ac  | jq .
 
 curl -X DELETE http://localhost:3000/ears/v1/routes/r123  | jq .
+
+curl -X DELETE http://localhost:3000/ears/v1/routes/f123  | jq .
 
 # idempotency test
 
