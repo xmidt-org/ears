90896a7efb52b69a5826db8f240cee2e51835dec
bwolf200 <mail@boriswolf.com>
2021-01-13 20:18:25 -0800

    tests

---
+++ cmd/ears/testdata/run.sh
@@ -0,0 +1,19 @@
+echo "add routes"
+
+curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRoute.json | jq .
+
+curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBlankID.json | jq .
+
+echo "get routes"
+
+curl -X GET http://localhost:3000/ears/v1/routes | jq .
+
+echo "delete routes"
+
+curl -X DELETE http://localhost:3000/ears/v1/routes/f7e3d975d08ae28005ac916dbeb5888e  | jq .
+
+curl -X DELETE http://localhost:3000/ears/v1/routes/r123  | jq .
+
+echo "get routes"
+
+curl -X GET http://localhost:3000/ears/v1/routes | jq .
