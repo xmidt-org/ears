b8cd2723cc71e414b225c9e738f5f0bdb42bb33e
bwolf200 <mail@boriswolf.com>
2021-01-25 13:48:58 -0800

    cleanup tests

--- cmd/ears/testdata/run.sh
+++ cmd/ears/testdata/run.sh
@@ -14,31 +14,31 @@
 
 echo "add routes"
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRoute.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRoute.json | jq .
 
 # idempotency test
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRoute.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRoute.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBlankID.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBlankID.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleFilterRoute.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleFilterRoute.json | jq .
 
 echo "invalid routes"
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBadName.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBadName.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBadPluginName.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteBadPluginName.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoReceiver.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoReceiver.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoSender.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoSender.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoApp.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoApp.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoOrg.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoOrg.json | jq .
 
-curl -X PUT http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoUser.json | jq .
+curl -X POST http://localhost:3000/ears/v1/routes --data @testdata/simpleRouteNoUser.json | jq .
 
 echo "get routes"
 
@@ -50,7 +50,7 @@ curl -X GET http://localhost:3000/ears/v1/routes/foo | jq .
 
 echo "delete routes"
 
-curl -X DELETE http://localhost:3000/ears/v1/routes/2abe4d167883f594f7b48ee7e1d247ac  | jq .
+curl -X DELETE http://localhost:3000/ears/v1/routes/94d5eff28471968e9bd946bc9db27847  | jq .
 
 curl -X DELETE http://localhost:3000/ears/v1/routes/r123  | jq .
 
