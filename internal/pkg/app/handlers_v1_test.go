894a159d520b900295df46502c21a7dc869177bc
bwolf200 <mail@boriswolf.com>
2021-01-27 18:50:24 -0800

    first rest unit test

--- internal/pkg/app/handlers_v1_test.go
+++ internal/pkg/app/handlers_v1_test.go
@@ -16,9 +16,19 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	goldie "github.com/sebdah/goldie/v2"
+	"github.com/xmidt-org/ears/internal/pkg/db"
+	"github.com/xmidt-org/ears/internal/pkg/plugin"
+	pkgplugin "github.com/xmidt-org/ears/pkg/plugin"
+	"github.com/xmidt-org/ears/pkg/plugin/manager"
+	"github.com/xmidt-org/ears/pkg/plugins/block"
+	"github.com/xmidt-org/ears/pkg/plugins/debug"
+	"github.com/xmidt-org/ears/pkg/plugins/match"
+	"github.com/xmidt-org/ears/pkg/plugins/pass"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -32,7 +42,6 @@ func TestVersionHandler(t *testing.T) {
 	}
 	api.versionHandler(w, r)
 	g := goldie.New(t)
-
 	var data interface{}
 	err = json.Unmarshal(w.Body.Bytes(), &data)
 	if err != nil {
@@ -44,15 +53,56 @@ func TestVersionHandler(t *testing.T) {
 func TestAddRouteHandler(t *testing.T) {
 	Version = "v1.0.2"
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest(http.MethodPut, "/v1/routes/testroute", nil)
-	api, err := NewAPIManager(&DefaultRoutingTableManager{})
+	simpleRouteReader, err := os.Open("testdata/route.golden")
 	if err != nil {
-		t.Fatalf("Fail to instaniate api manager: %s\n", err.Error())
+		t.Fatalf("cannot read testdata/route.golden")
+	}
+	r := httptest.NewRequest(http.MethodPost, "/v1/routes", simpleRouteReader)
+	inMemStorageMgr := db.NewInMemoryRouteStorer(nil)
+	mgr, err := manager.New()
+	if err != nil {
+		t.Fatalf("cannot create plugin manager: %s\n", err.Error())
+	}
+	pluginMgr, err := plugin.NewManager(plugin.WithPluginManager(mgr))
+	// register plugins
+	toArr := func(a ...interface{}) []interface{} { return a }
+	defaultPlugins := []struct {
+		name   string
+		plugin pkgplugin.Pluginer
+	}{
+		{
+			name:   "debug",
+			plugin: toArr(debug.NewPluginVersion("debug", "", ""))[0].(pkgplugin.Pluginer),
+		},
+		{
+			name:   "match",
+			plugin: toArr(match.NewPluginVersion("match", "", ""))[0].(pkgplugin.Pluginer),
+		},
+		{
+			name:   "pass",
+			plugin: toArr(pass.NewPluginVersion("pass", "", ""))[0].(pkgplugin.Pluginer),
+		},
+		{
+			name:   "block",
+			plugin: toArr(block.NewPluginVersion("block", "", ""))[0].(pkgplugin.Pluginer),
+		},
+	}
+	for _, plug := range defaultPlugins {
+		err = mgr.RegisterPlugin(plug.name, plug.plugin)
+		if err != nil {
+			t.Fatalf("could register %s plugin: %s", plug.name, err.Error())
+		}
+	}
+	//
+	routingMgr := NewRoutingTableManager(pluginMgr, inMemStorageMgr)
+	api, err := NewAPIManager(routingMgr)
+	if err != nil {
+		t.Fatalf("cannot create api manager: %s\n", err.Error())
 	}
 	api.addRouteHandler(w, r)
 	g := goldie.New(t)
-
 	var data interface{}
+	fmt.Printf("%s\n", string(w.Body.Bytes()))
 	err = json.Unmarshal(w.Body.Bytes(), &data)
 	if err != nil {
 		t.Fatalf("cannot unmarshal response %s into json %s", string(w.Body.Bytes()), err.Error())
