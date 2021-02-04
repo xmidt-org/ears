74bb53f576793355ab5e36564dcca8abe1e72a3c
bwolf200 <mail@boriswolf.com>
2021-02-04 12:44:37 -0800

    more multi route unit tests

--- pkg/plugins/debug/receiver.go
+++ pkg/plugins/debug/receiver.go
@@ -58,10 +58,13 @@ func (r *Receiver) Receive(ctx context.Context, next receiver.NextFn) error {
 		for count := *r.config.Rounds; count != 0; {
 			select {
 			case <-ctx.Done():
+				//fmt.Printf("RECEIVER DONE BY CTX\n")
 				return
 			case <-r.done:
+				//fmt.Printf("RECEIVER DONE BY ROUTE\n")
 				return
 			case <-time.After(time.Duration(*r.config.IntervalMs) * time.Millisecond):
+				//fmt.Printf("RECEIVER NEW EVENT\n")
 				e, err := event.NewEvent(r.config.Payload)
 				if err != nil {
 					return
@@ -148,6 +151,8 @@ func (r *Receiver) Trigger(ctx context.Context, e event.Event) error {
 	//BW TODO: where is the next slice here?
 	r.Lock()
 	next := r.next
+	//BW
+	//fmt.Printf("TRIGGER\n")
 	r.Unlock()
 
 	r.history.Add(e)
