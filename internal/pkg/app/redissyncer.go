c90bb3af68cb8d6b5454e6d4c5db6e26ab1d627e
bwolf200 <mail@boriswolf.com>
2021-02-22 10:15:13 -0800

    fine tuning redis logic and remove dead code

--- internal/pkg/app/redissyncer.go
+++ internal/pkg/app/redissyncer.go
@@ -39,7 +39,7 @@ type (
 )
 
 //TODO: add tests
-//TODO: remove ping logic
+//DONE: remove ping logic
 //DONE: integrate with table manager
 //TODO: load entire table on launch
 //DONE: logging
@@ -63,8 +63,6 @@ func NewRedisTableSyncer(routingTableMgr RoutingTableManager, logger *zerolog.Lo
 		DB:       0,
 	})
 	s.subscribers = make(map[string]int64)
-	//s.ListenForPingMessages()
-	//s.PublishPings()
 	s.ListenForSyncRequests()
 	logger.Info().Msg("Launching Redis Syncer")
 	return s
@@ -76,39 +74,43 @@ func (s *RedisTableSyncer) PublishSyncRequest(ctx context.Context, routeId strin
 	// listen for ACKs first ...
 	go func() {
 		numSubscribers := s.GetInstanceCount(ctx)
-		received := make(map[string]bool, 0)
-		lrc := redis.NewClient(&redis.Options{
-			Addr:     s.redisEndpoint,
-			Password: "",
-			DB:       0,
-		})
-		defer lrc.Close()
-		pubsub := lrc.Subscribe(EARS_REDIS_ACK_CHANNEL)
-		defer pubsub.Close()
-		// 30 sec timeout on collecting acks
-		done := make(chan bool, 1)
-		go func() {
-			for {
-				msg, err := pubsub.ReceiveMessage()
-				if err != nil {
-					s.logger.Error().Str("op", "PublishSyncRequest").Msg(err.Error())
-					break
-				} else {
-					s.logger.Info().Str("op", "PublishSyncRequest").Msg("receive ack on channel " + EARS_REDIS_ACK_CHANNEL)
+		if numSubscribers <= 1 {
+			s.logger.Info().Str("op", "PublishSyncRequest").Msg("no subscribers but me")
+		} else {
+			received := make(map[string]bool, 0)
+			lrc := redis.NewClient(&redis.Options{
+				Addr:     s.redisEndpoint,
+				Password: "",
+				DB:       0,
+			})
+			defer lrc.Close()
+			pubsub := lrc.Subscribe(EARS_REDIS_ACK_CHANNEL)
+			defer pubsub.Close()
+			// 30 sec timeout on collecting acks
+			done := make(chan bool, 1)
+			go func() {
+				for {
+					msg, err := pubsub.ReceiveMessage()
+					if err != nil {
+						s.logger.Error().Str("op", "PublishSyncRequest").Msg(err.Error())
+						break
+					} else {
+						s.logger.Info().Str("op", "PublishSyncRequest").Msg("receive ack on channel " + EARS_REDIS_ACK_CHANNEL)
+					}
+					received[msg.Payload] = true
+					// wait until we received an ack from each subscriber (except the one originating the request)
+					if len(received) >= numSubscribers-1 {
+						break
+					}
 				}
-				received[msg.Payload] = true
-				// wait until we received an ack from each subscriber (except the one originating the request)
-				if len(received) >= numSubscribers-1 {
-					break
-				}
+				done <- true
+			}()
+			select {
+			case <-done:
+				s.logger.Info().Str("op", "PublishSyncRequest").Msg("done collecting acks")
+			case <-time.After(30 * time.Second):
+				s.logger.Info().Str("op", "PublishSyncRequest").Msg("timeout while collecting acks")
 			}
-			done <- true
-		}()
-		select {
-		case <-done:
-			s.logger.Info().Str("op", "PublishSyncRequest").Msg("done collecting acks")
-		case <-time.After(30 * time.Second):
-			s.logger.Info().Str("op", "PublishSyncRequest").Msg("timeout while collecting acks")
 		}
 		s.PublishMutationMessage(ctx, routeId, add)
 	}()
@@ -161,22 +163,22 @@ func (s *RedisTableSyncer) ListenForSyncRequests() {
 				hostname, _ := os.Hostname()
 				if elems[2] != hostname {
 					if elems[0] == EARS_REDIS_ADD_ROUTE_CMD {
-						s.logger.Info().Str("op", "ListenForSyncRequests").Msg("received message to add route " + elems[1])
+						s.logger.Info().Str("op", "ListenForSyncRequests").Str("hostname", hostname).Msg("received message to add route " + elems[1])
 						err = s.routingTableMgr.SyncRouteAdded(ctx, elems[1])
 					} else if elems[0] == EARS_REDIS_REMOVE_ROUTE_CMD {
-						s.logger.Info().Str("op", "ListenForSyncRequests").Msg("received message to remove route " + elems[1])
+						s.logger.Info().Str("op", "ListenForSyncRequests").Str("hostname", hostname).Msg("received message to remove route " + elems[1])
 						err = s.routingTableMgr.SyncRouteRemoved(ctx, elems[1])
 					} else {
 						err = errors.New("bad command " + elems[0])
 					}
 					if err != nil {
-						s.logger.Error().Str("op", "ListenForSyncRequests").Msg(err.Error())
+						s.logger.Error().Str("op", "ListenForSyncRequests").Str("hostname", hostname).Msg(err.Error())
 					}
+					s.PublishAckMessage(ctx)
 				} else {
-					s.logger.Info().Str("op", "ListenForSyncRequests").Msg("no need to sync myself " + elems[2])
+					s.logger.Info().Str("op", "ListenForSyncRequests").Str("hostname", hostname).Msg("no need to sync myself")
 				}
 			}
-			s.PublishAckMessage(ctx)
 		}
 	}()
 }
@@ -193,57 +195,6 @@ func (s *RedisTableSyncer) PublishAckMessage(ctx context.Context) error {
 	}
 	return err
 }
-
-// PublishPings publish heart beat messages
-
-/*func (s *RedisTableSyncer) PublishPings() {
-	hostname, _ := os.Hostname()
-	go func() {
-		for {
-			err := s.client.Publish(EARS_REDIS_PING_CHANNEL, hostname).Err()
-			if err != nil {
-				s.logger.Error().Str("op", "PublishPings").Msg(err.Error())
-			} else {
-				s.logger.Info().Str("op", "PublishPings").Msg("published ping message for " + hostname)
-			}
-			time.Sleep(30 * time.Second)
-		}
-	}()
-}*/
-
-// ListenForPingMessages listens for heart beat messages
-
-/*func (s *RedisTableSyncer) ListenForPingMessages() {
-	go func() {
-		lrc := redis.NewClient(&redis.Options{
-			Addr:     s.redisEndpoint,
-			Password: "",
-			DB:       0,
-		})
-		defer lrc.Close()
-		pubsub := lrc.Subscribe(EARS_REDIS_PING_CHANNEL)
-		defer pubsub.Close()
-		for {
-			msg, err := pubsub.ReceiveMessage()
-			if err != nil {
-				s.logger.Error().Str("op", "ListenForPingMessages").Msg(err.Error())
-				time.Sleep(EARS_REDIS_RETRY_INTERVAL_SECONDS)
-			} else {
-				nowsec := time.Now().Unix()
-				if msg.Payload != "" {
-					s.subscribers[msg.Payload] = nowsec
-				}
-				// remove dead subscribers from list
-				for sid, ts := range s.subscribers {
-					if nowsec-ts > 120 {
-						delete(s.subscribers, sid)
-					}
-				}
-				s.logger.Info().Str("op", "ListenForPingMessages").Msg("processed ping message")
-			}
-		}
-	}()
-}*/
 
 // GetSubscriberCount gets number of live ears instances
 
