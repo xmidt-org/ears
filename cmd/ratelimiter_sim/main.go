e7cdbf2be9910f1e2fd5a1e27682172336dbf667
Michael Chiang <michael_chiang@cable.comcast.com>
2021-03-30 10:53:48 -0700

    Update the rate simulator to make reporting more concise

--- cmd/ratelimiter_sim/main.go
+++ cmd/ratelimiter_sim/main.go
@@ -20,16 +20,19 @@ func main() {
 	numWorker := 5
 	wg.Add(numWorker)
 
-	//for i := 0; i < numWorker; i++ {
-	go worker("0", &wg, 6, 30, 4, 20)
-	go worker("1", &wg, 6, 30, 4, 20)
-	go worker("2", &wg, 6, 30, 4, 20)
-	go worker("3", &wg, 6, 30, 4, 20)
-	go worker("4", &wg, 6, 30, 4, 20)
-	//}
+	duration := 10
+	quota := 1000
+
+	go worker("0", &wg, 300, duration, quota/numWorker, quota)
+	go worker("1", &wg, 300, duration, quota/numWorker, quota)
+	go worker("2", &wg, 100, duration, quota/numWorker, quota)
+	go worker("3", &wg, 100, duration, quota/numWorker, quota)
+	go worker("4", &wg, 20, duration, quota/numWorker, quota)
+
+	reporter(duration)
 	wg.Wait()
 
-	fmt.Printf("success %d, fail %d, error %d\n", successCount, failCount, errCount)
+	fmt.Printf("Total success %d, fail %d, error %d\n", successCount, failCount, errCount)
 }
 
 func ts(t time.Time) string {
@@ -56,13 +59,14 @@ func worker(workerName string, wg *sync.WaitGroup, rps int, second int, initialR
 		err := limiter.Take(ctx, 1)
 		end := time.Now()
 
-		fmt.Printf("(%s) start %s, end %s, (%s)\n", workerName, ts(start), ts(end), errStr(err))
+		//fmt.Printf("(%s) start %s, end %s, (%s)\n", workerName, ts(start), ts(end), errStr(err))
 		if err != nil {
+			//fmt.Printf("(%s) start %s, end %s, (%s)\n", workerName, ts(start), ts(end), errStr(err))
 			var limitReachErr *LimitReached
 			if errors.As(err, &limitReachErr) {
 				atomic.AddInt32(&failCount, 1)
 			} else {
-				fmt.Printf("error %s\n", err.Error())
+				fmt.Printf("(%s) start %s, end %s, (%s)\n", workerName, ts(start), ts(end), errStr(err))
 				atomic.AddInt32(&errCount, 1)
 			}
 			time.Sleep(time.Second / time.Duration(rps))
@@ -70,5 +74,21 @@ func worker(workerName string, wg *sync.WaitGroup, rps int, second int, initialR
 			time.Sleep(time.Second / time.Duration(rps))
 			atomic.AddInt32(&successCount, 1)
 		}
+	}
+}
+
+func reporter(duration int) {
+	var sCount int32 = 0
+	var fCount int32 = 0
+	var eCount int32 = 0
+	for i := 0; i < duration; i++ {
+		time.Sleep(time.Second)
+		sTmp := successCount
+		fTmp := failCount
+		eTmp := errCount
+		fmt.Printf("success %d, fail %d, error %d\n", sTmp-sCount, fTmp-fCount, eTmp-eCount)
+		sCount = sTmp
+		fCount = fTmp
+		eCount = eTmp
 	}
 }
