a1415ac0d2924a5dded08c83f753e557de57e702
bwolf200 <mail@boriswolf.com>
2021-04-16 08:27:43 -0700

    yaml support

--- internal/pkg/app/handlers_v1_test.go
+++ internal/pkg/app/handlers_v1_test.go
@@ -20,6 +20,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/goccy/go-yaml"
 	"github.com/rs/zerolog/log"
 	goldie "github.com/sebdah/goldie/v2"
 	"github.com/spf13/viper"
@@ -46,6 +47,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -181,11 +183,14 @@ func TestRouteTable(t *testing.T) {
 				// read and parse route
 				buf, err := ioutil.ReadFile("testdata/" + routeFileName + ".json")
 				if err != nil {
-					t.Fatalf("%s test: cannot read route file: %s", currentTestName, err.Error())
+					buf, err = ioutil.ReadFile("testdata/" + routeFileName + ".yaml")
+					if err != nil {
+						t.Fatalf("%s test: cannot read route file: %s", currentTestName, err.Error())
+					}
 				}
 				// scope route by prefixing all names (confirm with Trevor what unregister is meant to do)
 				var routeConfig route.Config
-				err = json.Unmarshal(buf, &routeConfig)
+				err = yaml.Unmarshal(buf, &routeConfig)
 				if err != nil {
 					t.Fatalf("%s test: cannot parse route: %s", currentTestName, err.Error())
 				}
@@ -530,10 +535,14 @@ func checkEventsSent(routeFileName string, testPrefix string, pluginMgr plugin.M
 	ctx = log.Logger.WithContext(ctx)
 	buf, err := ioutil.ReadFile(routeFileName)
 	if err != nil {
-		return err
+		routeFileName = strings.Replace(routeFileName, ".json", ".yaml", -1)
+		buf, err = ioutil.ReadFile(routeFileName)
+		if err != nil {
+			return err
+		}
 	}
 	var routeConfig route.Config
-	err = json.Unmarshal(buf, &routeConfig)
+	err = yaml.Unmarshal(buf, &routeConfig)
 	if err != nil {
 		return err
 	}
