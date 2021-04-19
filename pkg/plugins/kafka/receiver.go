de92b9e29b58e71c82aa9d914b9cc002c1a75287
bwolf200 <mail@boriswolf.com>
2021-04-19 11:07:00 -0700

    consumer integration

--- pkg/plugins/kafka/receiver.go
+++ pkg/plugins/kafka/receiver.go
@@ -14,9 +14,14 @@
 
 package kafka
 
+//TODO: worker pool
+
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"github.com/Shopify/sarama"
 	"github.com/rs/zerolog"
 	"os"
 	"time"
@@ -59,6 +64,113 @@ func NewReceiver(config interface{}) (receiver.Receiver, error) {
 	return r, nil
 }
 
+////
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (r *Receiver) Setup(session sarama.ConsumerGroupSession) error {
+	r.Lock()
+	r.ConsumerGroupSession = session
+	r.Unlock()
+	// Mark the consumer as ready
+	close(r.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (r *Receiver) Cleanup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (r *Receiver) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/master/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		//		log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		if r.handler(message) {
+			session.MarkMessage(message, "")
+		}
+	}
+	return nil
+}
+
+func (r *Receiver) Start(ctx context.Context, groupHandler sarama.ConsumerGroupHandler, handler func(*sarama.ConsumerMessage) bool) {
+	r.handler = handler
+	r.wg.Add(1)
+	defer r.wg.Done()
+	for {
+		// `Consume` should be called inside an infinite loop, when a
+		// server-side rebalance happens, the consumer session will need to be
+		// recreated to get the new claims
+		if err := r.client.Consume(r.ctx, r.topics, groupHandler); err != nil {
+			//ctx.Logger().Error("op", "Consumer.Start", "error", err)
+		}
+		// check if context was cancelled, signaling that the consumer should stop
+		if nil != r.ctx.Err() {
+			return
+		}
+		r.ready = make(chan bool)
+	}
+}
+
+func (r *Receiver) Close(ctx context.Context) {
+	<-r.ready // Await till the consumer has been set up
+	r.cancel()
+	r.wg.Wait()
+	if err := r.client.Close(); err != nil {
+		//ctx.Logger().Error("op", "Consumer.Close", "error", err)
+	}
+}
+
+////
+
+func (r *Receiver) getSaramaConfig(commitIntervalSec int) (*sarama.Config, error) {
+	// init (custom) config, enable errors and notifications
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+	config.Consumer.Offsets.AutoCommit.Interval = time.Duration(commitIntervalSec) * time.Second
+	// config.Group.Return.Notifications = true
+	// if conf.ConsumeByPartitions {
+	// 	config.Group.Mode = cluster.ConsumerModePartitions
+	// }
+	if "" != r.config.Version {
+		v, err := sarama.ParseKafkaVersion(r.config.Version)
+		if nil != err {
+			return nil, err
+		}
+		config.Version = v
+	}
+	if 0 < r.config.ChannelBufferSize {
+		config.ChannelBufferSize = r.config.ChannelBufferSize
+	}
+	config.Net.TLS.Enable = r.config.TLSEnable
+	if "" != r.config.Username {
+		config.Net.TLS.Enable = true
+		config.Net.SASL.Enable = true
+		config.Net.SASL.User = r.config.Username
+		config.Net.SASL.Password = r.config.Password
+	} else if "" != r.config.AccessCert {
+		keypair, err := tls.X509KeyPair([]byte(r.config.AccessCert), []byte(r.config.AccessKey))
+		if err != nil {
+			return nil, err
+		}
+		caAuthorityPool := x509.NewCertPool()
+		caAuthorityPool.AppendCertsFromPEM([]byte(r.config.CACert))
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{keypair},
+			RootCAs:      caAuthorityPool,
+		}
+		config.Net.TLS.Enable = true
+		config.Net.TLS.Config = tlsConfig
+	}
+	return config, nil
+}
+
+////
+
 func (r *Receiver) Receive(next receiver.NextFn) error {
 	if r == nil {
 		return &pkgplugin.Error{
@@ -76,6 +188,7 @@ func (r *Receiver) Receive(next receiver.NextFn) error {
 	r.done = make(chan struct{})
 	r.Unlock()
 	go func() {
+		//TODO: receive messages here
 	}()
 	r.logger.Info().Str("op", "kafka.Receive").Msg("waiting for receive done")
 	<-r.done
