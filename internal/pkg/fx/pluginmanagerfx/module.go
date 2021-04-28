be223328e4c1ddfb0522507b9a01d09ebdefea83
bwolf200 <mail@boriswolf.com>
2021-04-27 17:29:20 -0700

    hash plugin

--- internal/pkg/fx/pluginmanagerfx/module.go
+++ internal/pkg/fx/pluginmanagerfx/module.go
@@ -24,6 +24,7 @@ import (
 	"github.com/xmidt-org/ears/pkg/plugins/decode"
 	"github.com/xmidt-org/ears/pkg/plugins/dedup"
 	"github.com/xmidt-org/ears/pkg/plugins/encode"
+	"github.com/xmidt-org/ears/pkg/plugins/hash"
 	"github.com/xmidt-org/ears/pkg/plugins/js"
 	"github.com/xmidt-org/ears/pkg/plugins/kafka"
 	"github.com/xmidt-org/ears/pkg/plugins/match"
@@ -115,6 +116,10 @@ func ProvidePluginManager(in PluginIn) (PluginOut, error) {
 		{
 			name:   "encode",
 			plugin: toArr(encode.NewPluginVersion("encode", "", ""))[0].(pkgplugin.Pluginer),
+		},
+		{
+			name:   "hash",
+			plugin: toArr(hash.NewPluginVersion("hash", "", ""))[0].(pkgplugin.Pluginer),
 		},
 		{
 			name:   "block",
