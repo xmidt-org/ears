056d63f4f4a874378b3bacae26d68415d80fb9c3
bwolf200 <mail@boriswolf.com>
2021-06-08 14:09:41 -0700

    metrics and tracing for http plugin

--- pkg/plugins/http/receiver.go
+++ pkg/plugins/http/receiver.go
@@ -20,9 +20,15 @@ import (
 	"fmt"
 	"github.com/goccy/go-yaml"
 	"github.com/rs/zerolog"
+	"github.com/xmidt-org/ears/internal/pkg/rtsemconv"
 	"github.com/xmidt-org/ears/pkg/event"
 	pkgplugin "github.com/xmidt-org/ears/pkg/plugin"
 	"github.com/xmidt-org/ears/pkg/receiver"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/trace"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -50,12 +56,34 @@ func NewReceiver(config interface{}) (receiver.Receiver, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	logger := zerolog.New(os.Stdout).Level(zerolog.InfoLevel)
-	return &Receiver{
+	r := &Receiver{
 		config: cfg,
 		logger: &logger,
-	}, nil
+	}
+	// metric recorders
+	meter := global.Meter(rtsemconv.EARSMeterName)
+	commonLabels := []attribute.KeyValue{
+		attribute.String(rtsemconv.EARSPluginType, rtsemconv.EARSPluginTypeSQS),
+		attribute.String(rtsemconv.EARSAppIdLabel, "default"),
+		attribute.String(rtsemconv.EARSOrgIdLabel, "default"),
+	}
+	r.eventSuccessCounter = metric.Must(meter).
+		NewFloat64Counter(
+			rtsemconv.EARSMetricEventSuccess,
+			metric.WithDescription("measures the number of successful events"),
+		).Bind(commonLabels...)
+	r.eventFailureCounter = metric.Must(meter).
+		NewFloat64Counter(
+			rtsemconv.EARSMetricEventFailure,
+			metric.WithDescription("measures the number of unsuccessful events"),
+		).Bind(commonLabels...)
+	r.eventBytesCounter = metric.Must(meter).
+		NewInt64Counter(
+			rtsemconv.EARSMetricEventBytes,
+			metric.WithDescription("measures the number of event bytes processed"),
+		).Bind(commonLabels...)
+	return r, nil
 }
 
 func (h *Receiver) GetTraceId(r *http.Request) string {
@@ -63,46 +91,56 @@ func (h *Receiver) GetTraceId(r *http.Request) string {
 }
 
 func (h *Receiver) Receive(next receiver.NextFn) error {
-
 	mux := http.NewServeMux()
 	h.srv = &http.Server{Addr: fmt.Sprintf(":%d", *h.config.Port), Handler: mux}
-
 	mux.HandleFunc(h.config.Path, func(w http.ResponseWriter, r *http.Request) {
 		defer fmt.Fprintln(w, "good")
-
 		b, err := ioutil.ReadAll(r.Body)
 		defer r.Body.Close()
 		if err != nil {
 			h.logger.Error().Str("error", err.Error()).Msg("error reading body")
 			return
 		}
-
 		var body interface{}
 		err = json.Unmarshal(b, &body)
 		if err != nil {
 			h.logger.Error().Str("error", err.Error()).Msg("error unmarshalling body")
 			return
 		}
-		ctx := context.Background()
+		ctx := context.Background() // acknowledge timeout?
+		tracer := otel.Tracer(rtsemconv.EARSTracerName)
+		var span trace.Span
+		if *h.config.Trace {
+			ctx, span = tracer.Start(ctx, "httpReceiver")
+			span.SetAttributes(rtsemconv.EARSEventTrace)
+		}
+		h.eventBytesCounter.Add(ctx, int64(len(b)))
 		event, err := event.New(ctx, body, event.WithAck(func(e event.Event) {
-			//do nothing
+			if *h.config.Trace {
+				span.AddEvent("ack")
+				span.End()
+			}
+			h.eventSuccessCounter.Add(ctx, 1.0)
 		}, func(e event.Event, err error) {
 			h.logger.Error().Str("error", err.Error()).Msg("Nack handling events")
+			if *h.config.Trace {
+				span.AddEvent("nack")
+				span.RecordError(err)
+				span.End()
+			}
+			h.eventFailureCounter.Add(ctx, 1.0)
 		},
 		))
 		if err != nil {
 			h.logger.Error().Str("error", err.Error()).Msg("error creating event")
 		}
-
 		traceId := h.GetTraceId(r)
 		if traceId != "" {
 			subCtx := context.WithValue(event.Context(), "traceId", traceId)
 			event.SetContext(subCtx)
 		}
-
 		next(event)
 	})
-
 	return h.srv.ListenAndServe()
 }
 
