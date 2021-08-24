d2f2d429c95b3f4d40867fef821ee47c342b55d3
bwolf200 <mail@boriswolf.com>
2021-08-24 09:38:44 -0700

    float to int

--- pkg/plugins/debug/receiver.go
+++ pkg/plugins/debug/receiver.go
@@ -83,12 +83,12 @@ func NewReceiver(tid tenant.Id, plugin string, name string, config interface{}, 
 		attribute.String(rtsemconv.EARSOrgIdLabel, r.tid.OrgId),
 	}
 	r.eventSuccessCounter = metric.Must(meter).
-		NewFloat64Counter(
+		NewInt64Counter(
 			rtsemconv.EARSMetricEventSuccess,
 			metric.WithDescription("measures the number of successful events"),
 		).Bind(commonLabels...)
 	r.eventFailureCounter = metric.Must(meter).
-		NewFloat64Counter(
+		NewInt64Counter(
 			rtsemconv.EARSMetricEventFailure,
 			metric.WithDescription("measures the number of unsuccessful events"),
 		).Bind(commonLabels...)
@@ -140,12 +140,12 @@ func (r *Receiver) Receive(next receiver.NextFn) error {
 				e, err := event.New(ctx, r.config.Payload, event.WithAck(
 					func(evt event.Event) {
 						eventsDone.Done()
-						r.eventSuccessCounter.Add(ctx, 1.0)
+						r.eventSuccessCounter.Add(ctx, 1)
 						cancel()
 					}, func(evt event.Event, err error) {
 						r.logger.Error().Str("op", "debug.Receive").Msg("failed to process message: " + err.Error())
 						eventsDone.Done()
-						r.eventFailureCounter.Add(ctx, 1.0)
+						r.eventFailureCounter.Add(ctx, 1)
 						cancel()
 					}),
 					event.WithSpan(r.Name()),
