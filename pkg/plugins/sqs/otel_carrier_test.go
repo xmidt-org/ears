768aa2d7429ebd89d55ad7ba11ee673b19cff91b
Michael Chiang <michael_chiang@cable.comcast.com>
2021-09-24 10:05:33 -0700

    added one more test case to carrier unit test

--- pkg/plugins/sqs/otel_carrier_test.go
+++ pkg/plugins/sqs/otel_carrier_test.go
@@ -26,6 +26,12 @@ func TestSqsMessageAttributeCarrier(t *testing.T) {
 		t.Fatalf("Expect val2 for key2, got %s\n", val)
 	}
 
+	c.Set("key1", "val3")
+	val = c.Get("key1")
+	if val != "val3" {
+		t.Fatalf("Expect val3 for key1, got %s\n", val)
+	}
+
 	keys := c.Keys()
 	if len(keys) != 2 {
 		t.Fatalf("Expect 2 keys, got %d instead\n", len(keys))
