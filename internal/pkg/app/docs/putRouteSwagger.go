4553d8c286b02aabe19d3b6e8b8bd362f202b79d
bwolf200 <mail@boriswolf.com>
2021-09-29 11:09:28 -0700

    added tenant apis to openapi docs

--- internal/pkg/app/docs/putRouteSwagger.go
+++ internal/pkg/app/docs/putRouteSwagger.go
@@ -14,7 +14,7 @@
 
 package docs
 
-// swagger:route PUT /v1/orgs/{orgid}/applications/{appid}/routes/{routeId} routes putRoute
+// swagger:route PUT /v1/orgs/{orgId}/applications/{appId}/routes/{routeId} routes putRoute
 // Adds a new route to the routing table or updates an existing route. Route ID can be given in the body. If it is omitted a hash will be calculates and used instead.
 // responses:
 //   200: RouteResponse
