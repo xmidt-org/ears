ad4ca3eb297273ab4ba8df791bc1e61524efbc1f
bwolf200 <mail@boriswolf.com>
2021-10-08 15:08:04 -0700

    kinesis consumer name param

--- pkg/plugins/kinesis/receiver_config.go
+++ pkg/plugins/kinesis/receiver_config.go
@@ -68,6 +68,9 @@ const receiverSchema = `
                 "streamName": {
                     "type": "string"
                 },
+                "consumerName": {
+                    "type": "string"
+                },
                 "shardIteratorType": {
                     "type": "string"
                 },
