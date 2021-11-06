42e31a7daa3c03f3b3fd974a6f803b780d71c10f
bwolf200 <mail@boriswolf.com>
2021-11-05 20:11:18 -0700

    bug fix advanced transform

--- pkg/filter/transform/transform.go
+++ pkg/filter/transform/transform.go
@@ -73,49 +73,44 @@ func transform(evt event.Event, t interface{}, parent interface{}, key string, i
 	if evt == nil || t == nil {
 		return
 	}
-	switch t := t.(type) {
+	switch tt := t.(type) {
 	case map[string]interface{}:
-		for key, st := range t {
-			transform(evt, st, t, key, -1)
+		for key, st := range tt {
+			transform(evt, st, tt, key, -1)
 		}
 	case []interface{}:
-		for idx, st := range t {
-			transform(evt, st, t, "", idx)
+		for idx, st := range tt {
+			transform(evt, st, tt, "", idx)
 		}
 	case string:
 		for {
-			si := strings.Index(t, "{")
-			ei := strings.Index(t, "}")
+			si := strings.Index(tt, "{")
+			ei := strings.Index(tt, "}")
 			if si < 0 || ei < 0 {
 				break
 			}
-			path := t[si+1 : ei]
+			path := tt[si+1 : ei]
 			v, _, _ := evt.GetPathValue(path)
-			switch vt := v.(type) {
-			case string:
-				t = t[0:si] + vt + t[ei+1:]
-			default:
-				sv, _ := json.Marshal(vt)
-				t = t[0:si] + string(sv) + t[ei+1:]
+			if !(si == 0 && ei == len(tt)-1) {
+				switch vt := v.(type) {
+				case string:
+					tt = tt[0:si] + vt + tt[ei+1:]
+				default:
+					sv, _ := json.Marshal(vt)
+					tt = tt[0:si] + string(sv) + tt[ei+1:]
+				}
+				v = tt
+			} else {
+				tt = ""
 			}
 			if parent != nil {
 				if key != "" {
-					parent.(map[string]interface{})[key] = t
+					parent.(map[string]interface{})[key] = v
 				} else if idx >= 0 {
-					parent.([]interface{})[idx] = t
+					parent.([]interface{})[idx] = v
 				}
 			}
 		}
-		/*if strings.HasPrefix(t, "{") && strings.HasSuffix(t, "}") {
-			repl, _, _ := evt.GetPathValue(t[1 : len(t)-1])
-			if parent != nil {
-				if key != "" {
-					parent.(map[string]interface{})[key] = repl
-				} else if idx >= 0 {
-					parent.([]interface{})[idx] = repl
-				}
-			}
-		}*/
 	}
 }
 
