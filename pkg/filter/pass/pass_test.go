80fc0f6e527cb6cb77519f8db57053b24a61b89c
bwolf200 <mail@boriswolf.com>
2021-11-18 20:13:08 -0800

    unit tests for filters

---
+++ pkg/filter/pass/pass_test.go
@@ -0,0 +1,57 @@
+// Copyright 2021 Comcast Cable Communications Management, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pass_test
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/xmidt-org/ears/pkg/event"
+	"github.com/xmidt-org/ears/pkg/filter/pass"
+	"github.com/xmidt-org/ears/pkg/tenant"
+	"reflect"
+	"testing"
+)
+
+func TestFilterPassBasic(t *testing.T) {
+	ctx := context.Background()
+	f, err := pass.NewFilter(tenant.Id{AppId: "myapp", OrgId: "myorg"}, "match", "mymatch", pass.Config{}, nil)
+	if err != nil {
+		t.Fatalf("pass test failed: %s\n", err.Error())
+	}
+	eventStr := `{ "foo": "bar"}`
+	var obj interface{}
+	err = json.Unmarshal([]byte(eventStr), &obj)
+	if err != nil {
+		t.Fatalf("pass test failed: %s\n", err.Error())
+	}
+	e, err := event.New(ctx, obj, event.FailOnNack(t))
+	if err != nil {
+		t.Fatalf("pass test failed: %s\n", err.Error())
+	}
+	evts := f.Filter(e)
+	if len(evts) != 1 {
+		t.Fatalf("wrong number of passed events: %d\n", len(evts))
+	}
+	expectedEventStr := `{"foo":"bar"}`
+	var res interface{}
+	err = json.Unmarshal([]byte(expectedEventStr), &res)
+	if err != nil {
+		t.Fatalf("pass test failed: %s\n", err.Error())
+	}
+	if !reflect.DeepEqual(evts[0].Payload(), res) {
+		pl, _ := json.MarshalIndent(evts[0].Payload(), "", "\t")
+		t.Fatalf("wrong payload in passed event: %s\n", pl)
+	}
+}
