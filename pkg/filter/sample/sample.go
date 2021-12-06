266d3860ae0e399794524641758fb9b1af69a221
bwolf200 <mail@boriswolf.com>
2021-12-06 10:14:48 -0800

    sample filter

---
+++ pkg/filter/sample/sample.go
@@ -0,0 +1,79 @@
+// Copyright 2020 Comcast Cable Communications Management, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sample
+
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"github.com/xmidt-org/ears/pkg/event"
+	"github.com/xmidt-org/ears/pkg/filter"
+	"github.com/xmidt-org/ears/pkg/secret"
+	"github.com/xmidt-org/ears/pkg/tenant"
+	"math/rand"
+)
+
+func NewFilter(tid tenant.Id, plugin string, name string, config interface{}, secrets secret.Vault) (*Filter, error) {
+	cfg, err := NewConfig(config)
+	if err != nil {
+		return nil, &filter.InvalidConfigError{
+			Err: err,
+		}
+	}
+	cfg = cfg.WithDefaults()
+	err = cfg.Validate()
+	if err != nil {
+		return nil, err
+	}
+	f := &Filter{
+		config: *cfg,
+		name:   name,
+		plugin: plugin,
+		tid:    tid,
+	}
+	return f, nil
+}
+
+func (f *Filter) Filter(evt event.Event) []event.Event {
+	if f == nil {
+		evt.Nack(&filter.InvalidConfigError{
+			Err: fmt.Errorf("<nil> pointer filter"),
+		})
+		return nil
+	}
+	dice := rand.Float64()
+	log.Ctx(evt.Context()).Debug().Str("op", "filter").Str("filterType", "sample").Str("name", f.Name()).Msg("sample")
+	if dice <= *f.config.Percentage {
+		return []event.Event{evt}
+	}
+	return []event.Event{}
+}
+func (f *Filter) Config() interface{} {
+	if f == nil {
+		return Config{}
+	}
+	return f.config
+}
+
+func (f *Filter) Name() string {
+	return f.name
+}
+
+func (f *Filter) Plugin() string {
+	return f.plugin
+}
+
+func (f *Filter) Tenant() tenant.Id {
+	return f.tid
+}
