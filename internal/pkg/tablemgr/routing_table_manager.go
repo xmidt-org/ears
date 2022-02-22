228d2a06940a5fb1148447caef6ad8a5f992e542
Michael Chiang <michael_chiang@cable.comcast.com>
2022-02-22 13:01:28 -0800

    add more debug logs

--- internal/pkg/tablemgr/routing_table_manager.go
+++ internal/pkg/tablemgr/routing_table_manager.go
@@ -130,9 +130,15 @@ func (r *DefaultRoutingTableManager) registerAndRunRoute(ctx context.Context, ro
 	defer span.End()
 	var err error
 	// check if route already exists, check if this is an update etc.
+
+	r.logger.Info().Str("op", "registerAndRunRoute").Msg("mchiang debug - before liveRouteMap")
+
 	r.Lock()
 	existingLiveRoute, ok := r.liveRouteMap[routeConfig.TenantId.KeyWithRoute(routeConfig.Id)]
 	r.Unlock()
+
+	r.logger.Info().Str("op", "registerAndRunRoute").Msg("mchiang debug - after liveRouteMap")
+
 	if ok && existingLiveRoute.Config.Hash(ctx) == routeConfig.Hash(ctx) {
 		r.logger.Info().Str("op", "registerAndRunRoute").Str("routeId", routeConfig.Id).Msg("identical route exists with same hash and same ID")
 		return nil
@@ -146,6 +152,8 @@ func (r *DefaultRoutingTableManager) registerAndRunRoute(ctx context.Context, ro
 		}
 	}
 
+	r.logger.Info().Str("op", "registerAndRunRoute").Msg("mchiang debug - before entering lock")
+
 	// An identical route already exists under a different ID.
 	// It would be ok to simply create another route here because plugin manager will ensure we share receiver and sender
 	// plugin for performance. However, simply creating another route would cause event duplication. Instead we need to
@@ -156,7 +164,10 @@ func (r *DefaultRoutingTableManager) registerAndRunRoute(ctx context.Context, ro
 	// the route hash, use an internal reference counter and give up on idempotency. An alternative would be to take route creation
 	// out of the flow and use a dedicated route management UI.
 	r.Lock()
-	defer r.Unlock()
+	defer func() {
+		r.logger.Info().Str("op", "registerAndRunRoute").Msg("mchiang debug - exiting lock")
+		r.Unlock()
+	}()
 	existingLiveRoute, ok = r.routeHashMap[routeConfig.Hash(ctx)]
 	if ok {
 		// we simply increment the reference count of an already existing route and are done here
