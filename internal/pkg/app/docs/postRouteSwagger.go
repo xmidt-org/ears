3a421c158bff5d8d59582da93927503c014801b4
bwolf200 <mail@boriswolf.com>
2022-09-19 14:43:49 -0700

    swagger update

--- internal/pkg/app/docs/postRouteSwagger.go
+++ internal/pkg/app/docs/postRouteSwagger.go
@@ -14,8 +14,8 @@
 
 package docs
 
-// swagger:route POST /v1/orgs/{orgid}/applications/{appid}/routes routes postRoute
-// Adds a new route to the routing table or updates an existing route. Route ID can be given in the body. If it is omitted a hash will be calculates and used instead.
+// swagger:route POST /v1/orgs/{orgId}/applications/{appId}/routes routes postRoute
+// Adds a new route to the routing table or updates an existing route. Route ID can be given in the body. If it is omitted a hash will be calculated and used instead.
 // responses:
 //   200: RouteResponse
 //   500: RouteErrorResponse
