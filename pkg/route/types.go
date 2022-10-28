8f8c8a0555f2f78435bef7c37a4971ac394719b7
bwolf200 <mail@boriswolf.com>
2022-10-28 13:46:31 -0700

    add description field to route

--- pkg/route/types.go
+++ pkg/route/types.go
@@ -57,6 +57,7 @@ type Config struct {
 	TenantId     tenant.Id      `json:"tenant,omitempty"`       // TenantId. Derived from URL path. Should not be marshaled
 	UserId       string         `json:"userId,omitempty"`       // user ID / author of route
 	Name         string         `json:"name,omitempty"`         // optional unique name for route
+	Desc         string         `json:"desc,omitempty"`         // optional description for route
 	Origin       string         `json:"origin,omitempty"`       // optional reference to route owner, e.g. Flow ID in case of Gears
 	Receiver     PluginConfig   `json:"receiver,omitempty"`     // source plugin configuration
 	Sender       PluginConfig   `json:"sender,omitempty"`       // destination plugin configuration
