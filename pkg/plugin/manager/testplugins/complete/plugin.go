4b3162cff633477a20c3f16154c38ac12a9a1a9b
bwolf200 <mail@boriswolf.com>
2023-08-14 15:15:00 -0700

    unit test

--- pkg/plugin/manager/testplugins/complete/plugin.go
+++ pkg/plugin/manager/testplugins/complete/plugin.go
@@ -126,3 +126,19 @@ func (p *plugin) Tenant() tenant.Id {
 
 func (p *plugin) Trigger(e event.Event) {
 }
+
+func (p *plugin) EventSuccessCount() int {
+	return 0
+}
+
+func (p *plugin) EventSuccessVelocity() int {
+	return 0
+}
+
+func (p *plugin) EventErrorCount() int {
+	return 0
+}
+
+func (p *plugin) EventErrorVelocity() int {
+	return 0
+}
