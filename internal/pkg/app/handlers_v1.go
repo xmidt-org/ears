95a2b1e94e00f6a3ead26895bcbdded6c4dd2480
bwolf200 <boris_wolf@cable.comcast.com>
2024-07-26 15:26:57 -0700

    todo

--- internal/pkg/app/handlers_v1.go
+++ internal/pkg/app/handlers_v1.go
@@ -15,7 +15,7 @@
 //DONE: enhance event object to pass response back to sender plugin and return response
 //DONE: http sender needs more parameters: payload etc.
 //DONE: modify global webhook route to allow for optional route id parameter
-//TODO: http sender needs to support standard authentication methods (SAT, oauth etc.)
+//DONE: http sender needs to support standard authentication methods (SAT, oauth etc.)
 
 package app
 
